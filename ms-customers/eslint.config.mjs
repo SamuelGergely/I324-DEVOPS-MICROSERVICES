eab0339a77ecc8dd39d422644b8a7fa19b4f9405
Samuel Gergely <143069755+SamuelGergely@users.noreply.github.com>
2025-12-16 15:43:09 +0100

    ci: eslint now function as intented

--- ms-customers/eslint.config.mjs
+++ ms-customers/eslint.config.mjs
@@ -1,29 +1,11 @@
 import js from "@eslint/js";
-import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser }
-  },
-]);
-
-module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-  },
-  extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+export default defineConfig({
+  ...js.configs.recommended,
   rules: {
+    ...js.configs.recommended.rules,
     "no-unused-vars": "warn",
-    "no-console": "off"
-  }
-}
+    "no-console": "off",
+  },
+});
